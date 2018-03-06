cf81a1513c4a10d069c27d26986a9da2f46378c7
abrahamgnz <gnz.abr@gmail.com>
2018-03-06 12:58:11 -0600

    Agregar información a los componentes

--- README.md
+++ README.md
@@ -49,3 +49,16 @@ Un componente se compone de tres partes:
 **component class**: aquí se manejan los comportamientos, eventos y almacenamiento de información del template.
 
 **styles section**: para mejorar la apariencia del template.
+
+## Agregar información a los componentes
+
+
+Nuestros *todos* tendrán 3 componentes:
+
+  - title
+  - project
+  - done (para indicar si esta completo o no)
+
+Para pasar la información de nuestro componente principal a **TodoList** usaremos la directiva `v-bind`.
+
+Para hacer render de los items en **TodoList** usaremos la directiva `v-for="item in items"`.
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <TodoList/>
+    <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
 
@@ -12,6 +12,36 @@ export default {
   name: 'App',
   components: {
     TodoList
+  },
+  data () {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Todo A',
+          project: 'Project A',
+          done: false
+        },
+        {
+          id: 2,
+          title: 'Todo B',
+          project: 'Project B',
+          done: true
+        },
+        {
+          id: 3,
+          title: 'Todo C',
+          project: 'Project C',
+          done: false
+        },
+        {
+          id: 4,
+          title: 'Todo D',
+          project: 'Project D',
+          done: false
+        }
+      ]
+    }
   }
 }
 </script>
